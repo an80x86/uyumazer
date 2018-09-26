8412f46f496f05490d03720c1578c5d2f4eeb2f6
Mustafa Bayer <mustafabayer@Mustafa-MacBook-Pro.local>
2018-09-26 15:20:08 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import axios from 'axios';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  state = {
+    persons: []
+  }
+
+  async componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async() =>{
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  renderName() {
+    if (this.state.persons===null) return <Text>Yükleniyor..</Text>;
+    return this.state.persons.map((person, Id) =>
+      <Text key={Id} style={{ fontSize: 25, paddingTop: 20 }} > {person.name} </Text>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        { this.renderName() }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
